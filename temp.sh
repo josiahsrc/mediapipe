9e87012b3dd00458e3723854d30ead0f0777d032
josiahsrc <contact@josiahsaunders.com>
2021-04-18 22:26:43 -0600

    hand track works

--- temp.sh
+++ temp.sh
@@ -5,6 +5,13 @@ elif [[ $1 == "run" ]]
 then
     ./bazel-bin/mediapipe/examples/desktop/hand_bbox/hand_bbox \
         --calculator_graph_config_file=mediapipe/graphs/hand_bbox/hand_bbox_desktop_live.pbtxt
+elif [[ $1 == "build_ht" ]]
+then
+    bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hand_tracking:hand_tracking_cpu
+elif [[ $1 == "run_ht" ]]
+then
+    ./bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_cpu \
+        --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live.pbtxt
 else
     echo "Nope"
 fi
